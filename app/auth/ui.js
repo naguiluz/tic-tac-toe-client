89ffc5ab78ca60b53e5fb93d18e1269d947d2d85
naguiluz <nicolasaguiluz2@gmail.com>
2021-07-18 21:56:15 -0500

    Add ui changes on button clicks
    added new functionality so various ui elements will disappear and reappear intuitively

--- app/auth/ui.js
+++ app/auth/ui.js
@@ -2,10 +2,8 @@ const store = require('./../store')
 
 const onSignUpSuccess = function (response) { // this gives our user a success message
   $('#message').text(`Thanks for signing up, ${response.user.email}!`)
-  $('#sign-up').trigger('reset')
-  $('#sign-up').hide()
-  $('#sign-in').show()
-  $('#sign-out').hide()
+  $('#sign-up-header').css('display', 'none')
+  $('#sign-up').css('display', 'none')
 }
 
 const onSignInSuccess = function (response) {
@@ -13,17 +11,29 @@ const onSignInSuccess = function (response) {
   $('#message').text(`Welcome back, ${response.user.email}!`)
   $('#sign-up').trigger('reset')
   $('#sign-in').trigger('reset')
-  $('#sign-in').hide()
-  $('#sign-out').show()
+  $('#sign-out-header').css('display', 'block')
+  $('#sign-out').css('display', 'block')
+  $('#new-game-header').css('display', 'block')
+  $('#new-game').css('display', 'block')
+  $('#sign-in-header').css('display', 'none')
+  $('#sign-in').css('display', 'none')
+  $('#sign-up-header').css('display', 'none')
+  $('#sign-up').css('display', 'none')
 }
 
 const onSignOutSuccess = function () {
   $('#message').text('Come back soon!')
   $('#sign-up').trigger('reset')
   $('#sign-in').trigger('reset')
-  $('#sign-up').show()
-  $('#sign-in').show()
-  $('#sign-out').hide()
+  $('#sign-out-header').css('display', 'none')
+  $('#sign-out').css('display', 'none')
+  $('#new-game-header').css('display', 'none')
+  $('#new-game').css('display', 'none')
+  $('#sign-in-header').css('display', 'block')
+  $('#sign-in').css('display', 'block')
+  $('#sign-up-header').css('display', 'block')
+  $('#sign-up').css('display', 'block')
+  $('#game-board').css('display', 'none')
 }
 
 const onNewGameSuccess = function () {
